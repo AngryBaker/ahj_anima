f22e744e27d62d990883dea7feb51a0751444269
Angry Baker <checheninlive@mail.ru>
2025-04-06 13:19:26 +0300

    Initial commit

---
+++ src/js/Collapse.js
@@ -0,0 +1,22 @@
+export default class Collapse {
+  createCollapse() {
+    const collapseEl = document.createElement("div");
+    this.collapseEl = collapseEl;
+    collapseEl.className = "collapse";
+    collapseEl.innerHTML = `<button class="collapse_btn">Collapse</button>
+      <div class="collapse_content">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</div>
+    `;
+    document.body.appendChild(collapseEl);
+  }
+
+  addEvListeneer() {
+    const content = this.collapseEl.querySelector(".collapse_content");
+    this.collapseEl
+      .querySelector(".collapse_btn")
+      .addEventListener("click", (e) => {
+        e.preventDefault();
+        content.classList.toggle("show");
+      });
+    console.log(this.collapseEl.querySelector(".collapse_btn"));
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import Collapse from "./Collapse";
+
+const collapse = new Collapse();
+collapse.createCollapse();
+collapse.addEvListeneer();
